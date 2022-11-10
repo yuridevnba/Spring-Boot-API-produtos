d77a50b6d6aadea802049baa796d365a3a5f7f29
Yuri12 <yuri.00000830267@unicap.br>
2022-11-10 14:33:35 -0300

    Serviço para Cadastrar Produtos

--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,9 +1,11 @@
 package br.com.api.produtos.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.api.produtos.modelo.ProdutosModelo;
+import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.repositorio.ProdutoRepositorio;
 
 @Service
@@ -11,13 +13,18 @@ public class ProdutoServico {
 	
 	@Autowired
 	private ProdutoRepositorio pr;
+	@Autowired
+	private RespostaModelo rm;
 	
 	// Método para listar todos os produtos
 	// findALL()== select*from produtos
 	public  Iterable<ProdutosModelo> listar(){
 		return pr.findAll();
 		
-		
+		// Método para cadastrar produtos
+		public ResponseEntity<?>cadastrar(ProdutosModelo pm){
+			if(pm.g)
+		}
 	}
 
 }
