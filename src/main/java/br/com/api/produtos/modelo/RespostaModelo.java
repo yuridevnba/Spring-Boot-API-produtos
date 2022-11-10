59d88a118b52faaedadff6de0b89085f6a88b902
Yuri12 <yuri.00000830267@unicap.br>
2022-11-10 12:27:52 -0300

    Serviço para listar produtos

--- src/main/java/br/com/api/produtos/modelo/RespostaModelo.java
+++ src/main/java/br/com/api/produtos/modelo/RespostaModelo.java
@@ -1,5 +1,15 @@
 package br.com.api.produtos.modelo;
 
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// responsavel por retornar um feedback caso aconteça alguma falha na API, no cadastro, delete etc.
+@Component
+@Getter
+@Setter
 public class RespostaModelo {
 
+	private String mensagem;
 }
