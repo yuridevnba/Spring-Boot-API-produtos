df6c37d435c1fc7f0f8e5ba4a709ebb3af6dd177
Yuri12 <yuri.00000830267@unicap.br>
2022-11-10 12:16:23 -0300

    Configurando Controle e Modelo Pra Validar Requisições

--- src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,5 +1,14 @@
 package br.com.api.produtos.controle;
 
-public class ProdutoControle {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class ProdutoControle {
+      
+	@GetMapping("/")
+	public String rota() {
+		return "API de produtos funcionando!";
+	}
+	
 }
