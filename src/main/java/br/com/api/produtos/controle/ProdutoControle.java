090268a5e95d0f38bda0b5f22cbfc78bd0c662cc
Yuri12 <yuri.00000830267@unicap.br>
2022-11-10 20:29:48 -0300

    Rota para alterar produtos

--- src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +31,8 @@ public class ProdutoControle {
 	public ResponseEntity<?>cadastrar(@RequestBody ProdutosModelo pm){
 		return ps.cadastrarAlterar(pm,"cadastrar");
 	}
-	
+	@PutMapping("/alterar")
+	public ResponseEntity<?>alterar(@RequestBody ProdutosModelo pm){
+		return ps.cadastrarAlterar(pm,"alterar");
+	}
 }
